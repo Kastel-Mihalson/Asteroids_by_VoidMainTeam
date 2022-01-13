f16f35e86de00d3fd71c5a6991f224787282c5f8
ArtomStatsenko <namebyuser@gmail.com>
2022-01-13 09:44:48 +0300

    Глобальные GameObject -> локальные GameObject

--- Assets/Asteroids/Scripts/Controllers/Game/GameController.cs
+++ Assets/Asteroids/Scripts/Controllers/Game/GameController.cs
@@ -8,7 +8,6 @@ public class GameController : MonoBehaviour
     private ShipModel _shipModel;
     private ShipView _shipView;
     private ShipController _shipController;
-    private GameObject _shipGameObject;
     private GameObject _shipPrefab;
     private Vector3 _shipStartPosition;
 
@@ -16,7 +15,6 @@ public class GameController : MonoBehaviour
     private BulletModel _bulletModel;
     private BulletView _bulletView;
     private BulletController _bulletController;
-    private GameObject _bulletGameObject;
     private GameObject _bulletPrefab;
     private Transform _bulletSpawnPosition;
 
@@ -24,7 +22,6 @@ public class GameController : MonoBehaviour
     private AsteroidModel _asteroidModel;
     private AsteroidView _asteroidView;
     private AsteroidController _asteroidController;
-    private GameObject _asteroidGameObject;
     private GameObject _asteroidPrefab;
 
     // Game parameters
@@ -86,9 +83,10 @@ public class GameController : MonoBehaviour
         _shipStartPosition = new Vector3(0, 0, -4.5f);
         _shipPrefab = Resources.Load("PlayerV2") as GameObject;
         _shipPrefab.transform.localScale = new Vector3(0.6f, 0.6f, 0.6f);
-        _shipGameObject = Instantiate(_shipPrefab, _shipStartPosition, Quaternion.identity);
+        GameObject shipGameObject = Instantiate(_shipPrefab, _shipStartPosition, Quaternion.identity);
+
         _shipModel = new ShipModel();
-        _shipView = _shipGameObject.GetComponent<ShipView>();
+        _shipView = shipGameObject.GetComponent<ShipView>();
         _shipController = new ShipController(_shipModel, _shipView);
     }
 
@@ -106,7 +104,7 @@ public class GameController : MonoBehaviour
     }
 
     private void Shoot()
-    {      
+    {
         if (!_shipView)
         {
             return;
@@ -117,8 +115,10 @@ public class GameController : MonoBehaviour
         if (canShoot && isEnemyDetected)
         {
             _bulletModel.NextShoot = Time.time + _bulletModel.ShootDelay;
-            _bulletGameObject = Instantiate(_bulletPrefab, _bulletSpawnPosition.position, Quaternion.identity);
-            _bulletView = _bulletGameObject.GetComponent<BulletView>();
+
+            GameObject bulletGameObject = Instantiate(_bulletPrefab, _bulletSpawnPosition.position, Quaternion.identity);
+            _bulletView = bulletGameObject.GetComponent<BulletView>();
+
             _bulletController = new BulletController(_bulletModel, _bulletView);
             _bulletView.Die(_bulletModel.LifeTime);
             _bulletController.BulletFly();
@@ -130,8 +130,11 @@ public class GameController : MonoBehaviour
         if (Time.time > _asteroidModel.NextSpawn)
         {
             _asteroidModel.NextSpawn += Random.Range(_asteroidModel.MinDelay, _asteroidModel.MaxDelay);
-            _asteroidGameObject = Instantiate(_asteroidPrefab, new Vector3(Random.Range(_leftScreenBorder, _rightScreenBorder), 0, 8f), Quaternion.identity);
-            _asteroidView = _asteroidGameObject.GetComponent<AsteroidView>();
+            
+            GameObject asteroidGameObject = Instantiate(_asteroidPrefab, 
+                new Vector3(Random.Range(_leftScreenBorder, _rightScreenBorder), 0, 8f), Quaternion.identity);
+            
+            _asteroidView = asteroidGameObject.GetComponent<AsteroidView>();
             _asteroidController = new AsteroidController(_asteroidModel, _asteroidView);
             _asteroidController.AsteroidMove();
         }
