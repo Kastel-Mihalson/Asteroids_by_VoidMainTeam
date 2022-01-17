801e8017f20f232226a092393c85ba0d82f8ab03
AnnaG <iqshka@gmail.com>
2022-01-17 20:02:33 +0000

    GameObjectPool added

--- Assets/Asteroids/Scripts/Views/BulletView.cs
+++ Assets/Asteroids/Scripts/Views/BulletView.cs
@@ -1,11 +1,32 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class BulletView : MonoBehaviour, IInteractiveObject, IBullet
 {
     public Rigidbody Rigidbody => gameObject.GetComponent<Rigidbody>();
+    private GameObjectPool _bulletPool;
+    
+    public event Action<GameObject> ReturnBulletToPoolEvent;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var interactiveObject = other.gameObject.GetComponent<IInteractiveObject>();
+
+        if (interactiveObject is IAsteroid)
+        {
+            ReturnBulletToPool(gameObject);
+        }
+    }
 
     public void Die(float lifeTime)
     {
+        //yield return new WaitForSeconds(lifeTime);
+        //ReturnBulletToPool(gameObject);
         Destroy(gameObject, lifeTime);
     }
+
+    public void ReturnBulletToPool(GameObject bullet) => ReturnBulletToPoolEvent?.Invoke(bullet);
+    
 }
