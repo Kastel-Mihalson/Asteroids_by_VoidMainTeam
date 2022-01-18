68d28e4c53a7fc52075c3a855c082a8c4f51aef0
ArtomStatsenko <namebyuser@gmail.com>
2022-01-18 13:26:36 +0300

    Оптимизация
    1. Оптимизировал ShipController, GameController.
    2. EnemySpawnController - теперь просто SpawnController, корабли и астероиды (любые объекты в дальнейшем) создаются и инициируются там.
    3. Добавил MovementController, вынес из ShipControllera все движение туда

---
+++ Assets/Asteroids/Scripts/Controllers/MovementController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public sealed class MovementController
+{
+    private Rigidbody _rigidbody;
+    private ShipModel _shipModel;
+    private Transform _transform;
+    private float _offsetX;
+    private float _offsetZ;
+    private float _leftMovementLimit;
+    private float _rightMovementLimit;
+    private float _topMovementLimit;
+    private float _bottomMovementLimit;
+    private float _nextChangeDirectionTime;
+    private Vector3 _movement;
+    private ShipType _shipType;
+
+    public MovementController(Rigidbody rigidbody, ShipModel shipModel, CapsuleCollider collider, ShipType shipType)
+    {
+        _rigidbody = rigidbody;
+        _shipModel = shipModel;
+        _transform = _rigidbody.transform;
+        _offsetX = collider.radius;
+        _offsetZ = collider.height / 2;
+        _shipType = shipType;
+        _leftMovementLimit = GameModel.ScreenBorder[Border.Left] + _offsetX;
+        _rightMovementLimit = GameModel.ScreenBorder[Border.Right] - _offsetX;
+        _topMovementLimit = GameModel.ScreenBorder[Border.Top] - _offsetZ;
+        _bottomMovementLimit = GameModel.ScreenBorder[Border.Bottom] + _offsetZ;
+
+    }
+
+    private void GetInputMovementDirection()
+    {
+        float vertical = Input.GetAxis(AxisManager.VERTICAL);
+        float horizontal = Input.GetAxis(AxisManager.HORIZONTAL);
+        _movement = new Vector3(horizontal, 0, vertical);
+    }
+
+    private void GetRandomHorizontalMovementDirection()
+    {
+        _movement = new Vector3(Random.Range(-1f, 1f), 0f, 0f);
+    }
+
+    private void Move()
+    {
+        if (_rigidbody)
+        {
+            _rigidbody.velocity = _movement * _shipModel.MoveSpeed;
+            _rigidbody.rotation = Quaternion.Euler(0, 0, -_rigidbody.velocity.x * _shipModel.TurnSpeed);
+        }
+    }
+
+    private void LimitFlightArea()
+    {
+        if (_transform)
+        {
+            float x = Mathf.Clamp(_transform.position.x, _leftMovementLimit, _rightMovementLimit);
+            float z = Mathf.Clamp(_transform.position.z, _bottomMovementLimit, _topMovementLimit);
+            _transform.position = new Vector3(x, 0, z);
+        }
+    }
+
+    public void Execute()
+    {
+        if (_shipType == ShipType.Player)
+        {
+            GetInputMovementDirection();
+        }
+        else if (_shipType == ShipType.Enemy && Time.time > _nextChangeDirectionTime)
+        {
+            GetRandomHorizontalMovementDirection();
+            _nextChangeDirectionTime += Random.Range(1f, 5f);
+        }
+
+        LimitFlightArea();
+    }
+
+    public void FixedExecute()
+    {
+        Move();
+    }
+}
---
+++ Assets/Asteroids/Scripts/Controllers/SpawnController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController
+{
+    private AsteroidController _asteroidController;
+    private ShipController _shipController;
+    private float _nextSpawnTime;
+    private float _minSpawnDelay = 0.5f;
+    private float _maxSpawnDelay = 2f;
+
+    public void SpawnAsteroid(List<AsteroidData> asteroids)
+    {
+        if (Time.time > _nextSpawnTime)
+        {
+            var asteroidIndex = Random.Range(0, asteroids.Count);
+            _asteroidController = new AsteroidController(asteroids[asteroidIndex]);
+            _asteroidController.Init();
+            _asteroidController.Move();
+            _nextSpawnTime += Random.Range(_minSpawnDelay, _maxSpawnDelay);
+        }
+    }
+
+    public ShipController SpawnShip(ShipData shipData)
+    {
+        _shipController = new ShipController(shipData);
+        _shipController.Init(shipData.Type);
+        return _shipController;
+    }
+}
