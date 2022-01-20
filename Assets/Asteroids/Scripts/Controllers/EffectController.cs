01e473deae65416c1f1993b78ca9abbc3da82e80
ArtomStatsenko <namebyuser@gmail.com>
2022-01-21 00:11:31 +0300

    Добавил рут объект для эффектов

--- Assets/Asteroids/Scripts/Controllers/EffectController.cs
+++ Assets/Asteroids/Scripts/Controllers/EffectController.cs
@@ -2,11 +2,14 @@
 
 public sealed class EffectController
 {
+    private const string EFFECT = "Effect";
     private static EffectData _effectData;
+    private static GameObject _root;
 
     public EffectController(EffectData effectData)
     {
         _effectData = effectData;
+        _root = new GameObject($"[{EFFECT}]");
     }
 
     public static void Init(EffectManager effect, Transform root)
@@ -16,6 +19,7 @@ public sealed class EffectController
         GameObject effectGameObject = Object.Instantiate(prefab);        
         effectGameObject.transform.position = root.transform.position;
         effectGameObject.transform.rotation = root.transform.rotation;
+        effectGameObject.transform.SetParent(_root.transform);
         float lifeTime = _effectData.Effects.GetEffectTime(effect);
         Object.Destroy(effectGameObject, lifeTime);
     }
