2cf87d4f57423fe10ca8011006e256649197dba1
Kastel-Mihalson <constanto147@yandex.ru>
2022-01-23 22:38:40 +0300

    ABV-27 Добавлена полоска жизни и брони

--- Assets/Asteroids/Scripts/Controllers/ShipController.cs
+++ Assets/Asteroids/Scripts/Controllers/ShipController.cs
@@ -16,19 +16,32 @@ public sealed class ShipController
 
     public Transform BulletStartPoint => _bulletStartPoint;
 
+    private HealthBar _health;
+    private ArmorBar _armor;
+    private ShipType _shipType;
+
     public ShipController(ShipData data)
     {
         _data = data;
         _startPosition = data.StartPosition;
         _prefab = data.ShipPrefab;
+        _health = Object.FindObjectOfType<HealthBar>();
+        _armor = Object.FindObjectOfType<ArmorBar>();
     }
 
     public void Init(ShipType type)
     {
+        _shipType = type;
         _model = new ShipModel(_data);
         GameObject shipGameObject = Object.Instantiate(_prefab, _startPosition, Quaternion.identity);
         _view = shipGameObject.GetComponent<ShipView>();
         _bulletStartPoint = _view.BulletSpawnPoint;
+
+        if (_shipType == ShipType.Player)
+        {
+            _health.SetMaxHealth(_model.MaxHP);
+            _armor.SetMaxArmor(_model.MaxArmor);
+        }
 
         if (shipGameObject.TryGetComponent(out CapsuleCollider collider))
         {
@@ -67,10 +80,20 @@ public sealed class ShipController
         if (_model.CurrentArmor > 0)
         {
             _model.CurrentArmor -= damage;
+
+            if (_shipType == ShipType.Player)
+            {
+                _armor.SetArmor(_model.CurrentArmor);
+            }
         }
         else
         {
             _model.CurrentHP -= damage;
+
+            if (_shipType == ShipType.Player)
+            {
+                _health.SetHealth(_model.CurrentHP);
+            }
         }
 
         if (_model.CurrentHP <= 0)
