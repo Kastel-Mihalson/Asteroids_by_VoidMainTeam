709f922f24b564d7827df6ea587b9aed8d5aae12
ArtomStatsenko <namebyuser@gmail.com>
2022-01-24 14:50:51 +0300

    Добавил функционал на Menu
    1. Меню с настройками: общая громкость (пока только музыка в главном меню) ,разрешение экрана и полноэкранная игра, качество графики.
    2. Выход из игры
    3. Загрузка сцены с игрой
    4. Музыка на фоне главного меню

--- Assets/Asteroids/Scripts/UI/MainMenu.cs
+++ Assets/Asteroids/Scripts/UI/MainMenu.cs
@@ -1,30 +1,38 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public sealed class MainMenuController : MonoBehaviour
+public sealed class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button _startButton;
     [SerializeField] private Button _settingsButton;
     [SerializeField] private Button _quitButton;
+    [SerializeField] private GameObject _settingsPanel;
 
     private void Start()
     {
         _startButton.onClick.AddListener(StartGame);
         _settingsButton.onClick.AddListener(OpenSettings);
         _quitButton.onClick.AddListener(QuitGame);
+
+        // AudioController.Play(AudioClipManager.NewGameMusic, true);
     }
 
     private void StartGame()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     private void OpenSettings()
     {
-
+        if (!_settingsPanel.activeSelf)
+        {
+            _settingsPanel.SetActive(true);
+            gameObject.SetActive(false);
+        }
     }
 
-    private void QuitGame()
+        private void QuitGame()
     {
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
